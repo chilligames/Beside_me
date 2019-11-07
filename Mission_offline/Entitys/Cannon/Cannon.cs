17e6765056d5019ad2cc8495d695acc3d27a64c5
Hossynkoala <Hossynassghary@gmail.com>
2019-11-07 15:12:07 +0330

    change cannon

--- Mission_offline/Entitys/Cannon/Cannon.cs
+++ Mission_offline/Entitys/Cannon/Cannon.cs
@@ -83,6 +83,7 @@ public class Cannon : MonoBehaviour
 
     IEnumerator Start_cannon_fire()
     {
+        //anim start
         while (true)
         {
             yield return new WaitForSeconds(0.01f);
@@ -93,9 +94,32 @@ public class Cannon : MonoBehaviour
             }
         }
 
-        yield return new WaitForSeconds(1);
-        print("spawn");
-        Instantiate(Raw_bullet, place_cannon.transform.position, transform.rotation).AddComponent<Bullet>().Chnage_value(new Bullet.Setting_bullet { All_place = Setting.All_place, place_cannon = place_cannon, Target_place_Postion = Finde_target() });//change value palce pos
+        //job
+        while (true)
+        {
+            if (Setting.Magezin >= 1)
+            {
+                Instantiate(Raw_bullet, place_cannon.transform.position, transform.rotation).AddComponent<Bullet>().Chnage_value(new Bullet.Setting_bullet { All_place = Setting.All_place, place_cannon = place_cannon, Target_place_Postion = Finde_target(), place_For = Setting.Place_for });//change value palce pos
+                Setting.Magezin -= 1;
+            }
+            else if (Setting.Magezin <= 0)
+            {
+                break;
+            }
+            yield return new WaitForSeconds(10);
+        }
+
+        //anim destory;
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+            transform.localScale = Vector3.MoveTowards(transform.localScale, Vector3.zero, 0.01f);
+            if (transform.localScale == Vector3.zero)
+            {
+                Destroy(gameObject);
+            }
+
+        }
 
     }
 
@@ -105,30 +129,120 @@ public class Cannon : MonoBehaviour
 
     class Bullet : MonoBehaviour
     {
+
         Setting_bullet Setting;
+
+
+        GameObject[] place_for_destory;
+
 
         public void Chnage_value(Setting_bullet setting_Bullet)
         {
             Setting = setting_Bullet;
+
+
+            //finde place for destroy
+            place_for_destory = new GameObject[5];
+            var Count_destroy = 0;
+            foreach (var item in Setting.All_place)
+            {
+                if (Vector3.Distance(item.transform.position, setting_Bullet.Target_place_Postion.transform.position) <= 0.8f)
+                {
+                    for (int i = 0; i < place_for_destory.Length; i++)
+                    {
+                        if (place_for_destory[i] == null)
+                        {
+                            place_for_destory[i] = item;
+                            Count_destroy++;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            var new_place = new GameObject[Count_destroy];
+            foreach (var item in place_for_destory)
+            {
+                if (item != null)
+                {
+                    for (int i = 0; i < new_place.Length; i++)
+                    {
+                        if (new_place[i] == null)
+                        {
+                            new_place[i] = item;
+                            break;
+                        }
+                    }
+
+                }
+
+            }
+
+            place_for_destory = new_place;
+
+
+            //start job
             StartCoroutine(Start_bullet());
         }
-
-
 
         IEnumerator Start_bullet()
         {
             while (true)
             {
-
                 yield return new WaitForSeconds(0.01f);
 
                 Vector3 diff = Setting.place_cannon.transform.position - transform.position;
                 diff.Normalize();
-                float rot_z = Mathf.Atan2(diff.y,diff.x) * Mathf.Rad2Deg;
-                transform.rotation = Quaternion.Euler(0, 0, rot_z-180);
+                float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.Euler(0, 0, rot_z - 180);
 
                 transform.position = Vector3.MoveTowards(transform.position, Setting.Target_place_Postion.transform.position, 0.01f);
+
+                if (transform.position == Setting.Target_place_Postion.transform.position)
+                {
+                    break;
+                }
             }
+
+            while (true)
+            {
+                yield return new WaitForSeconds(0.01f);
+
+                transform.localScale = Vector3.MoveTowards(transform.localScale, Vector3.zero, 0.01f);
+
+                if (transform.localScale == Vector3.zero)
+                {
+                    foreach (var item in place_for_destory)
+                    {
+                        switch (Setting.place_For)
+                        {
+                            case Place_for.Enemy:
+                                {
+                                    if (item.GetComponent<Place>().Setting_place.Type_place != Type_place.Enemy && item.GetComponent<Place>().Setting_place.Type_place != Type_place.Player && item.GetComponent<Place>().Setting_place.Place_for != Place_for.Enemy)
+                                    {
+                                        item.GetComponent<Place>().Count = 0;
+                                        item.GetComponent<Place>().Setting_place.Place_for = Place_for.Empity;
+                                        item.GetComponent<Place>().Setting_place.Type_place = Type_place.Place;
+                                    }
+                                }
+                                break;
+                            case Place_for.Player:
+                                {
+                                    if (item.GetComponent<Place>().Setting_place.Type_place != Type_place.Enemy && item.GetComponent<Place>().Setting_place.Place_for != Place_for.Player && item.GetComponent<Place>().Setting_place.Place_for != Place_for.Player)
+                                    {
+                                        item.GetComponent<Place>().Count = 0;
+                                        item.GetComponent<Place>().Setting_place.Place_for = Place_for.Empity;
+                                        item.GetComponent<Place>().Setting_place.Type_place = Type_place.Place;
+                                    }
+                                }
+                                break;
+                        }
+
+                    }
+                    Destroy(gameObject);
+                }
+            }
+
         }
         public struct Setting_bullet
         {
@@ -136,6 +250,7 @@ public class Cannon : MonoBehaviour
             public GameObject place_cannon;
 
             public GameObject[,] All_place;
+            public Place_for place_For;
 
         }
     }
@@ -150,6 +265,7 @@ public class Cannon : MonoBehaviour
     }
 
 }
+
 
 
 
